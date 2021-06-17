c16007eba4935b958a72cc0c1a720d1e3a69b282
zhang lian zhuang <zlianzhuang@163.com>
2021-06-18 01:50:09 +0800

    update go.mod

--- go.mod
+++ go.mod
@@ -3,9 +3,9 @@ module github.com/DataWorkbench/sourcemanager
 go 1.15
 
 require (
-	github.com/DataWorkbench/common v0.0.0-20210616064428-7fa8704914b5
+	github.com/DataWorkbench/common v0.0.0-20210617164914-7a8b5e7a06cd
 	github.com/DataWorkbench/glog v0.0.0-20201114060240-9471edb2b8cf
-	github.com/DataWorkbench/gproto v0.0.0-20210616061035-72bebe09571b
+	github.com/DataWorkbench/gproto v0.0.0-20210616112334-ce28349d3323
 	github.com/DataWorkbench/loader v0.0.0-20201119073611-6f210eb11a8c
 	github.com/Shopify/sarama v1.27.2
 	github.com/aws/aws-sdk-go v1.27.0
--- tests/sourcemanager_test.go
+++ tests/sourcemanager_test.go
@@ -615,14 +615,14 @@ func Test_SotUpdate(t *testing.T) {
 	require.Equal(t, errorCode(err), qerror.ResourceAlreadyExists.Code())
 }
 
-func Test_PrepareDelete(t *testing.T) {
-	managerDelete(t, "", true)
-}
-
-func Test_SotDelete(t *testing.T) {
-	tablesDelete(t, "")
-}
-
-func Test_Delete(t *testing.T) {
-	managerDelete(t, "", false)
-}
+//func Test_PrepareDelete(t *testing.T) {
+//	managerDelete(t, "", true)
+//}
+//
+//func Test_SotDelete(t *testing.T) {
+//	tablesDelete(t, "")
+//}
+//
+//func Test_Delete(t *testing.T) {
+//	managerDelete(t, "", false)
+//}
